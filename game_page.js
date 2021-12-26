382da5d01c411a1d3daf0ef8dfa0d9ef8f40c7ba
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2021-12-25 23:49:49 -0800

    answer calc

--- game_page.js
+++ game_page.js
@@ -11,31 +11,9 @@ document.getElementById("player_question").innerHTML = "Question Turn: " + playe
 document.getElementById("player_answer").innerHTML = "Answer Turn: " + player2_name;
 
 function send(){
-    get_word = document.getElementById("word").value;
-    word = get_word.toLowerCase();
-
-    charAt1 = word.charAt(1);
-    console.log(charAt1);
-
-    length_divide = Math.floor(word.length/2);
-    charAt2 = word.charAt(length_divide);
-    console.log(charAt2);
-
-    length_minus_one = word.length - 1;
-    charAt3 = word.charAt(length_minus_one);
-    console.log(charAt3);
-
-    remove_charAt1 = word.replace(charAt1,"_");
-    remove_charAt2 = remove_charAt1.replace(charAt2,"_");
-    remove_charAt3 = remove_charAt2.replace(charAt3,"_");
-    console.log(remove_charAt3);
-
-    question_word = "<h4 id='word_display'> Q. "+remove_charAt3+"</h4>";
-    input_box = "<br>Answer : <input type='text' id='input_check_box'>";
-    check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
-    row =  question_word + input_box + check_button ;
-    document.getElementById("output").innerHTML = row;
-    document.getElementById("word").value = "";
+  number1 = document.getElementById("number1").value;
+  number2 = document.getElementById("number2").value;
+  actual_answer = parseInt(number1) * parseInt(number2);
 }
 question_turn = "player1";
 answer_turn = "player2";
